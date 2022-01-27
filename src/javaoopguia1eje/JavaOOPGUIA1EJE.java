55a3791ada9f342edbf5186a7854290ac3bf4113
leandropala <leandropalavicino@gmail.com>
2022-01-26 21:57:08 -0300

    primer commit

---
+++ src/javaoopguia1eje/JavaOOPGUIA1EJE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoopguia1eje;
+
+/**
+ *
+ * @author Pedrin
+ */
+public class JavaOOPGUIA1EJE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
